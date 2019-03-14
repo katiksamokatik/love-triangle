2ae5b81b8949b11db9ed249a374ab63e401e07d0
Katya <kat.lapitskaya@gmail.com>
2019-03-15 00:46:33 +0300

    feature: add function which counts possible triangles

--- src/index.js
+++ src/index.js
@@ -4,4 +4,17 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
-};
+  let count = 0;  
+  for (let i = 0; i < preferences.length; i++) { 
+      let one = preferences[i] - 1;
+      if (one == i) continue;
+      let two = preferences[one] - 1;
+      if (two == one) continue;
+      let three = preferences[two] - 1;
+      if (three == two) continue;
+      if (three == i) {
+          count++;
+      }
+   }
+   return count / 3;
+  };
